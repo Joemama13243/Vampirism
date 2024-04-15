0dc0a02623f48608dbef0fcf835b5e388f787a81
cheaterpaul <dev@paube.de>
2024-04-15 21:59:27 +0200

    add blood grinder jei recipes

--- src/main/java/de/teamlapen/vampirism/client/gui/screens/BloodGrinderScreen.java
+++ src/main/java/de/teamlapen/vampirism/client/gui/screens/BloodGrinderScreen.java
@@ -2,6 +2,7 @@ package de.teamlapen.vampirism.client.gui.screens;
 
 import de.teamlapen.vampirism.REFERENCE;
 import de.teamlapen.vampirism.inventory.BloodGrinderMenu;
+import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.network.chat.Component;
@@ -11,7 +12,7 @@ import org.jetbrains.annotations.NotNull;
 
 public class BloodGrinderScreen extends AbstractContainerScreen<BloodGrinderMenu> {
 
-    private static final ResourceLocation background = new ResourceLocation(REFERENCE.MODID, "textures/gui/grinder.png");
+    private static final ResourceLocation BACKGROUND = new ResourceLocation(REFERENCE.MODID, "textures/gui/grinder.png");
 
     public BloodGrinderScreen(@NotNull BloodGrinderMenu inventorySlotsIn, @NotNull Inventory playerInventory, @NotNull Component name) {
         super(inventorySlotsIn, playerInventory, name);
@@ -26,6 +27,17 @@ public class BloodGrinderScreen extends AbstractContainerScreen<BloodGrinderMenu
     @Override
     protected void renderBg(@NotNull GuiGraphics graphics, float var1, int var2, int var3) {
         graphics.setColor(1, 1, 1, 1);
-        graphics.blit(background, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight);
+        graphics.blit(BACKGROUND, this.leftPos, this.topPos, 0, 0, this.imageWidth, this.imageHeight);
+        graphics.blitSprite(new ResourceLocation(REFERENCE.MODID, "container/grinder/progress_background"), this.leftPos + 80, this.topPos + 55, 16,16);
+
+        if (this.menu.hasItem()) {
+            int i = Minecraft.getInstance().levelRenderer.getTicks()/10 % 4;
+            graphics.blitSprite(new ResourceLocation(REFERENCE.MODID, switch (i) {
+                case 0 -> "container/grinder/progress_0";
+                case 1 -> "container/grinder/progress_1";
+                case 2 -> "container/grinder/progress_2";
+                default -> "container/grinder/progress_3";
+            }), this.leftPos + 80, this.topPos + 55, 16, 16);
+        }
     }
 }
--- src/main/java/de/teamlapen/vampirism/inventory/BloodGrinderMenu.java
+++ src/main/java/de/teamlapen/vampirism/inventory/BloodGrinderMenu.java
@@ -2,12 +2,18 @@ package de.teamlapen.vampirism.inventory;
 
 import de.teamlapen.lib.lib.inventory.InventoryContainerMenu;
 import de.teamlapen.vampirism.api.VampirismAPI;
+import de.teamlapen.vampirism.api.datamaps.IItemBlood;
 import de.teamlapen.vampirism.core.ModContainer;
+import de.teamlapen.vampirism.core.ModFluids;
+import net.minecraft.core.Direction;
 import net.minecraft.world.Container;
 import net.minecraft.world.SimpleContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.item.ItemStack;
+import net.neoforged.neoforge.capabilities.Capabilities;
+import net.neoforged.neoforge.fluids.FluidStack;
+import net.neoforged.neoforge.fluids.capability.IFluidHandler;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.function.Predicate;
@@ -25,5 +31,10 @@ public class BloodGrinderMenu extends InventoryContainerMenu {
     public BloodGrinderMenu(int id, @NotNull Inventory playerInventory, @NotNull Container inventory, ContainerLevelAccess worldPosIn) {
         super(ModContainer.BLOOD_GRINDER.get(), id, playerInventory, worldPosIn, inventory, SELECTOR_INFOS);
         this.addPlayerSlots(playerInventory);
+    }
+
+    public boolean hasItem() {
+        ItemStack item = this.inventory.getItem(0);
+        return !item.isEmpty();
     }
 }
